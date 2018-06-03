9c749c50d44e9d7416e8eaf5fc730006e9265500
Brian Badahdah <adambadahdah@gmail.com>
2018-06-02 19:32:07 -0700

    Trying to fix setup.py

--- setup.py
+++ setup.py
@@ -12,5 +12,6 @@ setup(name='StructPy',
           'numpy',
           'matplotlib'],
       zip_safe=False,
-      packages=['StructPy.StructPy', 'StructPy.Resources'])
+      #packages=['StructPy', 'Resources'],
+      py_modules=['StructPy', 'Resources'])
  
