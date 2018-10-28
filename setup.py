609dc9657095043ab5201a4979cc06962a2c2263
Brian Badahdah <adambadahdah@gmail.com>
2018-10-28 14:09:02 -0700

    Deleted build folder
    Conflicts:
    	StructPy/materials.py
    	build/lib/StructPy/materials.py

--- setup.py
+++ setup.py
@@ -13,11 +13,6 @@ setup(name='StructPy',
           'numpy',
           'matplotlib'],
       zip_safe=False,
-      packages=['Resources'],
-      py_modules=['structural_classes',
-      					  'Truss',
-      					  'steel_classes',
-      					  'materials',
-      					  'Frame',
-      					  'Loads'])
+      packages=['Resources','StructPy']
+      )
  
